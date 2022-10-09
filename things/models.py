29aa141c48240bfef9e71fb61fee2fb57fa573c3
zard01 <areesha.durrani@hotmail.com>
2022-10-09 21:34:37 +0100

    Fix Things model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-class Thing():
+class Thing(AbstractUser):
     name =models.CharField(max_length=15)
     description=models.CharField()
     quantity = models.IntegerField()
