c0684617337ffcd30bb293bb8d9c067d6390ec7c
Areesha <areesha.durrani@hotmail.com>
2022-10-10 10:28:41 +0100

    Extend Things model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.core.validators import RegexValidator
 from django.db import models
 
-class Thing():
+class Thing(models.Model):
     name =models.CharField(max_length=15)
     description=models.CharField()
     quantity = models.IntegerField()
