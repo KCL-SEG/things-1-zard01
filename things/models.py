bbf28f0e14ffc056bd3228650917c70549cb6dbf
Areesha <areesha.durrani@hotmail.com>
2022-10-10 10:37:43 +0100

    Extend Things model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,8 @@
 from django.core.validators import RegexValidator
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
-    name =models.CharField(max_length=15)
-    description=models.CharField()
-    quantity = models.IntegerField()
+    name =models.CharField(max_length=30, unique =True, blank = False)
+    description=models.CharField(max_length=120)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
